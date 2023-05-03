c66b0c003394115decefb5d8bef53b58b54cf3d7
TOLOES <tomdehame@orange.fr>
2023-05-03 10:44:35 +0200

    Modification pour prendre en compte le tableau de

--- src/UPJV/Validator/ValidatorEngine.php
+++ src/UPJV/Validator/ValidatorEngine.php
@@ -12,42 +12,27 @@ namespace UPJV\Validator;
  * à partir de sa liste. La validation est déclenchée par l'envoi
  * du message run( data ) et return si la donnée est valide ou non.
  */
+
 class ValidatorEngine
 {
     protected array $setOfValidator;
 
-    /**
-     * Charge les validateurs décrit dans le fichier JSON passé en argument.
-     *
-     * @param $json
-     */
     public function __construct($json)
     {
         $json = json_decode($json, true);
         foreach ($json as $name => $contraintes) {
-            foreach ($contraintes as $class => $param) {
+            foreach ($contraintes as $class => $params) {
                 include_once __DIR__ . "/ValidatorInterface.php";
                 include_once __DIR__ . "/$class.php";
                 $className = '\UPJV\Validator\\' . $class;
-                /**
-                 * Pour l'autocompletion dans PHPStorm
-                 *
-                 * @var ValidatorInterface $validator
-                 */
-                $validator = new $className();
-                $this->setOfValidator[$name][] = $validator->build($param);
+                foreach ($params as $param) {
+                    $validator = new $className();
+                    $this->setOfValidator[$name][] = $validator->build($param);
+                }
             }
         }
     }
 
-    /**
-     * Déclanche la validation d'un ensemble de donnée $inputs
-     * Exemple $engine->run($_POST); vérifie toutes les données du formulaire
-     *
-     * @param array $inputs
-     *
-     * @return bool
-     */
     public function run(array $inputs): bool
     {
         foreach ($inputs as $name => $val) {
@@ -67,3 +52,4 @@ class ValidatorEngine
         return true;
     }
 }
+
